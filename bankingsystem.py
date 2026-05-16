de16fc6b34e864fd966be7ecf6c0aa79e6977fa5
Krishnal Patil <krishpatil0018@gmail.com>
2026-05-16 22:49:58 +0530

    Update bankingsystem.py

--- bankingsystem.py
+++ bankingsystem.py
@@ -1,4 +1,4 @@
-#logic behind the calculation is an automatica
+#logic behind the calculation is an automati
 class BankAccount:      
     def __init__(self, name, balance):
         self.name = name
